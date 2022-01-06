8535e3c658527ff8c3a3f06b48e69499be270e99
MandumulaShivani <85151123+MandumulaShivani@users.noreply.github.com>
2022-01-06 17:54:35 +0530

    Update LargestNumber.java

--- Training/LargestInArray/LargestNumber.java
+++ Training/LargestInArray/LargestNumber.java
@@ -1,13 +1,13 @@
 package LargestInArray;
 
 public class LargestNumber {
-    public static void main(String args[]){
-        int array[] = {66,45,88,22,2,7};
-        int temp=array[0];
-        for(int i = 0; i<array.length; i++ ){
-           if(array[i]>temp)
-           temp=array[i];
+    public static void main(String args[]) {
+        int array[] = { 66, 45, 88, 22, 2, 7 };
+        int temp = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > temp)
+                temp = array[i];
         }
-        System.out.println("Largest Number in the array is:"+ temp);
+        System.out.println("Largest Number in the array is:" + temp);
     }
 }
