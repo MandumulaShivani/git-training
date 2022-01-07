173a5afbbe1fb9f0fa4b296cfb9127a29bd132bc
MandumulaShivani <mshiva3009@gmail.com>
2022-01-07 14:36:59 +0530

    Practice

---
+++ Training/Practice/Egg.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class Egg {
+    public static void printPattern(String pattern){
+        for (int i = 1; i <= 1; i++) {
+            System.out.println(pattern);
+        }
+    }
+    public static void main(String args[]) {
+        printPattern("  _______");
+        printPattern(" /       \\");
+        printPattern("/         \\");
+        printPattern("-\"-'-\"-'-\"-");
+        printPattern("\\         /");
+        printPattern(" \\_______/");
+    } 
+}
