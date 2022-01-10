767a6bb0d8a79f7dbf2bd753be8470ebe2150212
MandumulaShivani <mshiva3009@gmail.com>
2022-01-10 10:59:50 +0530

    Practice

---
+++ Training/Practice/Egg2.java
@@ -0,0 +1,33 @@
+package Practice;
+
+public class Egg2 {
+    public static void printPattern(String pattern){
+        for (int i = 1; i <= 1; i++) {
+            System.out.println(pattern);
+        }
+    }
+    public static void main(String args[]) {
+        printPattern("  _______");
+        printPattern(" /       \\");
+        printPattern("/         \\");
+        printPattern("\\         /");
+        printPattern(" \\_______/");
+        printPattern("-\"-'-\"-'-\"-");
+
+        printPattern("  _______");
+        printPattern(" /       \\");
+        printPattern("/         \\");
+        printPattern("\\         /");
+        printPattern(" \\_______/");
+        printPattern("-\"-'-\"-'-\"-");
+        printPattern("\\         /");
+        printPattern(" \\_______/");
+
+        printPattern("  _______");
+        printPattern(" /       \\");
+        printPattern("/         \\");
+        printPattern("-\"-'-\"-'-\"-");
+        printPattern("\\         /");
+        printPattern(" \\_______/");
+    } 
+}
---
+++ Training/Practice/Egg3.java
@@ -0,0 +1,74 @@
+package Practice;
+
+public class Egg3 {
+    public static void printPattern(String pattern){
+        for (int i = 1; i <= 1; i++) {
+            System.out.println(pattern);
+        }
+    }
+    public static void main(String args[]) {
+        for(int i= 1;i <=1;i++){
+            System.out.println("  _______");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println(" /       \\");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println("/         \\");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println("\\         /");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println(" \\_______/");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println("-\"-'-\"-'-\"-");
+        }
+
+
+        for(int i= 1;i <=1;i++){
+            System.out.println("  _______");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println(" /       \\");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println("/         \\");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println("\\         /");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println(" \\_______/");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println("-\"-'-\"-'-\"-");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println("\\         /");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println(" \\_______/");
+        }
+
+        for(int i= 1;i <=1;i++){
+            System.out.println("  _______");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println(" /       \\");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println("/         \\");
+        }
+        for(int i= 1;i <=1;i++){
+                System.out.println("-\"-'-\"-'-\"-");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println("\\         /");
+        }
+        for(int i= 1;i <=1;i++){
+            System.out.println(" \\_______/");
+        }
+    }
+}
